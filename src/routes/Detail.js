686cece280e1da1b38a3acecbd375ae79d56a04c
Dragonkeun <Dragonkeun7@gmail.com>
2023-01-06 05:59:52 +0900

    7.6 Detail까지 완료

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,8 +1,34 @@
-import { useParams } from "react-router-dom"
-function Detail(){
-    const {id} = useParams();
-    console.log(id);
-    return <h1>Detail</h1>
-    
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+function Detail() {
+  const [loading, setLoading] = useState(true);
+  const [movie, setMovie] = useState("");
+  const { id } = useParams();
+  const getMovie = async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+    console.log(json.data.movie);
+    setMovie(json.data.movie);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMovie();
+  }, []);
+  return (
+    <div>
+      {loading ? (
+        <div>Loading ...</div>
+      ) : (
+        <div>
+          <img src={movie.medium_cover_image} />
+          <br />
+          {movie.title}
+          <br />
+          {movie.description_full}
+        </div>
+      )}
+    </div>
+  );
 }
 export default Detail;
